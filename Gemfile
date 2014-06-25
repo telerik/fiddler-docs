fde6d18e09b97ce7617bdfcb4668933e97056c43
Atanas Korchev <akorchev@gmail.com>
2014-06-25 17:17:56 +0300

    Use html pipeline for markdown conversion.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 gem 'jekyll'
 gem 'jekyll-sitemap'
+gem 'github-markdown'
+gem 'html-pipeline'
--- _plugins/markdown_processor.rb
+++ _plugins/markdown_processor.rb
@@ -63,13 +63,79 @@ module Jekyll
       end
     end
 
+    require 'github/markup'
+    require 'html/pipeline'
+
+    class RootRelativeFilter < HTML::Pipeline::Filter
+
+        def call
+            doc.search('a').each do |a|
+                next if a['href'].nil?
+
+                href = a['href'].strip
+
+                if href.start_with? '/'
+                    a['href'] = context[:baseurl] +  href
+                end
+            end
+
+            doc.search('img').each do |img|
+                next if img['src'].nil?
+                src = img['src'].strip
+                if src.start_with? '/'
+                    img['src'] = context[:baseurl] + src
+                end
+            end
+
+            doc
+        end
+
+    end
+
+    # based on https://github.com/jch/html-pipeline/blob/master/lib/html/pipeline/toc_filter.rb
+    class HeaderLinkFilter < HTML::Pipeline::Filter
+
+        PUNCTUATION_REGEXP = RUBY_VERSION > "1.9" ? /[^\p{Word}\- ]/u : /[^\w\- ]/
+
+        def call
+
+            doc.css('h1, h2, h3').each do |node|
+                next if node['id']
+
+                id = node.text.downcase
+                id.gsub!(PUNCTUATION_REGEXP, '') # remove punctuation
+                id.gsub!(' ', '-') # replace spaces with dash
+
+                node['id'] = id
+
+                a = Nokogiri::XML::Node.new 'a', doc
+                a['href'] = "##{id}"
+                a.children = node.children
+                node.add_child a
+            end
+
+            doc
+        end
+    end
+
     class Converters::Markdown::MarkdownProcessor
         def initialize(config)
             @config = config
+
+            context = {
+                :baseurl => @config['baseurl']
+            }
+
+            @pipeline = HTML::Pipeline.new [
+                HTML::Pipeline::MarkdownFilter,
+                RootRelativeFilter,
+                HeaderLinkFilter
+            ], context
+
         end
 
         def convert(content)
-            Kramdown::Document.new(content, @config['kramdown'].merge({ :baseurl => @config['baseurl'] })).to_custom_html
+            @pipeline.call(content)[:output].to_s
         end
     end
 
