6425f8bc912bc9d67ed70b270d7a498d7a1feff7
Stefan Dobrev <Stefan.Dobrev@telerik.com>
2014-07-01 14:16:26 +0300

    Warn when a slug with no matching page is found.

--- _plugins/slug.rb
+++ _plugins/slug.rb
@@ -6,7 +6,12 @@ class SlugTag < Liquid::Block
     def render(context)
         site = context.registers[:site]
         page = site.pages.find {|p| p.data['slug'] == @text }
-        page.url.sub('.html', '')
+        if page
+            page.url.sub('.html', '')
+        else
+            page_url = context.environments.first["page"]["url"]
+            Jekyll.logger.warn "Slug:", "No page with slug `#{@text}` in #{page_url}. Consider fixing the slug or use normal link." 
+        end
     end
 end
 
