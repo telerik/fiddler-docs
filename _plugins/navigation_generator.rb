9508864a734d55b7cf76cbc7d75f398534abcf47
Atanas Korchev <akorchev@gmail.com>
2014-07-07 12:34:09 +0300

    Ignore pages with publish: false

--- _plugins/navigation_generator.rb
+++ _plugins/navigation_generator.rb
@@ -11,6 +11,7 @@ module Jekyll
             site.pages.each do |page|
                 category = page.data['category']
 
+                next if page.data['publish'] == false
                 next unless category
 
                 node = categories[category]
