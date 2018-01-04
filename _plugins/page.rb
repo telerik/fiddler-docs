8d796207a8e97e867301603a0b7c549143ae8844
Genady <genady.sergeev@progress.com>
2018-01-04 17:25:52 +0200

    refactor: updated fiddler-docs to jekyll3 .5 and jekyll-assets 3.0

--- _plugins/page.rb
+++ _plugins/page.rb
@@ -2,18 +2,22 @@ module Jekyll
     class Page
         #https://github.com/jekyll/jekyll/blob/master/lib/jekyll/convertible.rb#L44
         def read_yaml(base, name, opts = {})
+			filename = File.join(base, name)
+			
             begin
-                self.content = File.read(Jekyll.sanitized_path(base, name),
-                                         merged_file_read_opts(opts))
+                self.content = File.read(@path || site.in_source_dir(base, name),
+                                         Utils.merged_file_read_opts(site, opts))
                 if content =~ /\A(---\s*\n.*?\n?)^((---|\.\.\.)\s*$\n?)/m
                     self.content = $POSTMATCH
                     self.data = SafeYAML.load($1.gsub(/{{site\.([^}]+)}}/) {|o| site.config[$1]} )
                 end
-            rescue SyntaxError => e
-                Jekyll.logger.warn "YAML Exception reading #{File.join(base, name)}: #{e.message}"
-            rescue Exception => e
-                Jekyll.logger.warn "Error reading file #{File.join(base, name)}: #{e.message}"
-            end
+            rescue Psych::SyntaxError => e
+				Jekyll.logger.warn "YAML Exception reading #{filename}: #{e.message}"
+				raise e if self.site.config["strict_front_matter"]
+			rescue StandardError => e
+				Jekyll.logger.warn "Error reading file #{filename}: #{e.message}"
+				raise e if self.site.config["strict_front_matter"]
+			end
 
             self.data ||= {}
         end
